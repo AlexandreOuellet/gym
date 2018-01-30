1dfed5c02b12acc454d334dff2d99a9d5f14be70
Alexandre Ouellet <AOuellet@square-enix-montreal.com>
2018-01-30 12:50:00 -0500

    Added pickle import

--- examples/agents/cem.py
+++ examples/agents/cem.py
@@ -8,6 +8,7 @@ import json, sys, os
 from os import path
 from _policies import BinaryActionLinearPolicy # Different file so it can be unpickled
 import argparse
+import pickle
 
 def cem(f, th_mean, batch_size, n_iter, elite_frac, initial_std=1.0):
     """
